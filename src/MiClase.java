50e1ff867c24c742d545419055e10a432233f8b4
Miguel-Paredes <os.miguelparedes@gmail.com>
2023-07-07 11:52:36 -0500

    update

--- src/MiClase.java
+++ src/MiClase.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class MiClase {
+import java.io.Serializable;
+
+public class MiClase implements Serializable {
+    private static final long serialVersionUID=1L;
+    private String nombre;
+    private int edad;
+    public MiClase(String nombre, int edad){
+        this.nombre=nombre;
+        this.edad=edad;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    @Override
+    public String toString(){
+        return "Nombre:"+nombre+",edad:"+edad;
+
+    }
 }
--- src/principal.java
+++ src/principal.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class principal {
+import java.io.*;
+
+public class principal {
+    public static void main(String[] args) {
+        String filePath="datos.dat";
+        MiClase miObjeto = new MiClase("Miguel",19);
+        try (FileOutputStream fileOut=new FileOutputStream(filePath);
+             ObjectOutputStream obOut=new ObjectOutputStream(fileOut);
+        ) {
+            obOut.writeObject(miObjeto);
+            System.out.println("archivo escrito correctamente");
+        }catch(IOException e){
+            throw new RuntimeException(e);
+        }
+
+        try(FileInputStream fileIn=new FileInputStream(filePath);
+            ObjectInputStream obIn= new ObjectInputStream(fileIn);){
+            MiClase readObject= new MiClase();
+        }catch(IOException e){
+            throw new RuntimeException(e);
+        }
+
+
+    }
 }
